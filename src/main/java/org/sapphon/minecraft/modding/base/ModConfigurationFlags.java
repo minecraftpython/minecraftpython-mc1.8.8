cad9421c52c350da68302d121a2d1916c7f776dd
unknown <sapphon@gmail.com>
2019-06-25 22:50:08 -0400

    Turn off MP for web for now, the text version is the priority

--- src/main/java/org/sapphon/minecraft/modding/base/ModConfigurationFlags.java
+++ src/main/java/org/sapphon/minecraft/modding/base/ModConfigurationFlags.java
@@ -4,5 +4,5 @@ public class ModConfigurationFlags {
 	public static boolean SPELLCRAFTERS(){return true;}
 	public static boolean MINECRAFT_PYTHON_PROGRAMMING(){return true;}
 	public static boolean STUDENT_MODS(){return false;}
-	public static boolean MPPM_WEB() {return true;}
+	public static boolean MPPM_WEB() {return false;}
 }
